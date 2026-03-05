ea3c66db6415b0d6971a164106153827d5b1352e
Jean Lukas de Marins Costa  do Nascimento <lukasjean745@gmail.com>
2026-03-05 13:50:54 -0300

    Modified

---
+++ CursoPython/GerenciadorDeTarefas/gerenciador.py
@@ -0,0 +1,57 @@
+def adicionar_tarefa():
+    """Função de adiciona uma tarefa a uma lista de tarefas, caso a lista esteja vazia, isso é tratado e informado ao 
+    Usuário """
+    pass
+
+def visualizar_tarefa():
+    pass
+
+def atualizar_tarefa():
+    pass
+
+def completar_tarefa():
+    pass
+
+def deletar_tarefa():
+    pass
+
+
+def main():
+    
+    while True:
+        
+        print("Bem Vindo ao nosso Sistema de gerenciamento de Tarefas!")
+        print("Escolha uma das opções a seguir:")
+        print("OPÇÃO 1 - ADICIONAR TAREFA")
+        print("OPÇÃO 2 - VISUALIZAR TAREFA")
+        print("OPÇÃO 3 - ATUALIZAR TAREFA")
+        print("OPÇÃO 4 - COMPLETAR TAREFA")
+        print("OPÇÃO 5 - DELETAR TAREFA")
+        print("OPÇÃO 6 - SAIR")
+        
+
+        opcao = int(input("Digite o numero da opção que deseja: "))
+        
+        if opcao == 1:
+            pass
+        
+        if opcao == 2:
+            pass
+        
+        if opcao == 3:
+            pass
+            
+        if opcao == 4:
+            pass
+        
+        if opcao == 5:
+            pass
+        
+        if opcao == 6:
+            print("Obrigado Por Utilizar Nosso Sistema!")
+            print("Saindo...")
+            break
+
+if __name__ == "__main__":
+    main()
+    
