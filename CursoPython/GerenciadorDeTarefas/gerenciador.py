92b56127241fd9863022621665eb7ed3c7b8ecd8
Jean Lukas de Marins Costa  do Nascimento <lukasjean745@gmail.com>
2026-03-06 12:53:10 -0300

    Added the final functions of the project such as delete task, rename tasks and finish task

--- CursoPython/GerenciadorDeTarefas/gerenciador.py
+++ CursoPython/GerenciadorDeTarefas/gerenciador.py
@@ -18,18 +18,52 @@ def visualizar_tarefa():
         print("Lista vazia!")
     for tarefa in lista_de_tarefas:
         print(f"""
-              Titulo: {tarefa["titulo"]} 
+              Titulo: {tarefa["titulo"]}
               Assunto: {tarefa["sobre"]}
               """)
 
-def atualizar_tarefa():
-    pass
+def atualizar_tarefa(titulo:str):
+    for tarefa in lista_de_tarefas:
+        if titulo in tarefa["titulo"]:
+            try:
+                pergunta = int(input("""Oque Deseja Atualizar? 
+                                        1 - TITULO
+                                        2 - ASSUNTO  """))
+                if pergunta == 1:
+                    novo_titulo = input("Digite um novo titulo para sua tarefa: ")
+                    tarefa["titulo"] = novo_titulo
+                    
+                elif pergunta == 2:
+                    novo_assunto = input("Digite um novo assunto para sua tarefa: ")
+                    tarefa["sobre"] = novo_assunto
+                    
+                else:
+                    print("Porfavor insira um número inteiro e que esteja dentro das opções!")
+                
+            except ValueError:
+                print("Insira um número inteiro!")
+            
+def completar_tarefa(titulo:str):
+    for tarefa in lista_de_tarefas:
+        if titulo == tarefa["titulo"]:
+            concluir_tarefa = input("Deseja concluir esta tarefa? s/n")
+            
+            if concluir_tarefa == 's':
+                lista_de_tarefas.remove(tarefa)
+                print(f"tarefa '{titulo}' concluída com sucesso!")
+                return
+            
+    print("Tarefa não encontrada!")
+    
 
-def completar_tarefa():
-    pass
-
-def deletar_tarefa():
-    pass
+def deletar_tarefa(titulo:str):
+    for tarefa in lista_de_tarefas:
+        if titulo == tarefa["titulo"]:
+            lista_de_tarefas.remove(tarefa)
+            print(f"Tarefa com titulo '{titulo}' deletada")
+            return
+    
+    print("Tarefa não encontrada!")
 
 
 def main():
@@ -67,13 +101,16 @@ def main():
                 visualizar_tarefa()
             
             elif opcao == 3:
-                pass
+                titulo = input("Digite o Titulo da tarefa que deseja atualizar: ")
+                atualizar_tarefa(titulo)
                 
             elif opcao == 4:
-                pass
+                titulo = input("Digite o titulo da tarefa que deseja concluir: ")
+                completar_tarefa(titulo)
             
             elif opcao == 5:
-                pass
+                titulo = input("Digite o titulo da tarefa que deseja deletar: ")
+                deletar_tarefa(titulo)
             
             elif opcao == 6:
                 print("Obrigado Por Utilizar Nosso Sistema!")
